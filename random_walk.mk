f6909e3ac73a29e399d0e39cabaa1dba42a3d74b
Ivan Burbano <im.burbano10@uniandes.edu.co>
2017-04-18 07:41:02 -0500

    Ejercicio 4 con makefile

---
+++ random_walk.mk
@@ -0,0 +1,10 @@
+all : rand.png
+
+rand.png : random_walks.txt
+	python random_walk.py
+
+random_walks.txt : random_walk.x
+	./random_walk.x
+
+random_walk.x : random_walk.c
+	cc random_walk.c -lm -o random_walk.x
